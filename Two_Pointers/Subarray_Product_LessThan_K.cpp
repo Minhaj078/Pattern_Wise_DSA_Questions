739bcebe5ae1b61131f0d7fe13a266a70527614a
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-12-22 20:23:48 +0530

    Improve sliding window implementation for subarray product
    Refactor sliding window logic for clarity and correctness.

--- Two_Pointers/Subarray_Product_LessThan_K.cpp
+++ Two_Pointers/Subarray_Product_LessThan_K.cpp
@@ -1,3 +1,5 @@
+//SlidingWindow Approach
+
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
@@ -9,12 +11,12 @@ public:
         if(k<=1)return 0;
         
         while(j < n){
-            product *= nums[j];
-            while(product >= k){
-                product /= nums[i];
-                i++;
+            product *= nums[j]; //
+            while(product >= k){ 
+                product /= nums[i]; //If the product >=k then nums[i] joki starting mai hogi usse divide kr ke window shrink kr do
+                i++; // and then window ko aage bdhao
             }
-            cnt += (j - i + 1);
+            cnt += (j - i + 1); //it will covers all the contigous subarrays
             j++;        
         }
         return cnt;        
