201350fa6491a6075b8915adc153ca7766d817d4
{minhaj} <{rockrizwanahmed@gmail.com}>
2025-12-24 23:03:19 +0530

    Added 4Sum

--- Two_Pointers/3Sums.cpp
+++ Two_Pointers/3Sums.cpp
@@ -2,24 +2,30 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>ans;
+
         sort(nums.begin(),nums.end());
-        for(int i = 0;i<nums.size();i++){
-            if(i > 0 && nums[i] == nums[i-1])continue;
-            int left = i + 1, right = n-1;
+        vector<vector<int>>ans;
+        for(int i = 0;i<n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int left = i + 1, right = n - 1;
+
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum == 0){
                     ans.push_back({nums[i],nums[left],nums[right]});
-                    while(left < right && nums[left] == nums[left+1])left++;
-                    while(left < right && nums[right] == nums[right-1])right--;
                     left++;
                     right--;
+                    
+                    while(left < right && nums[left] == nums[left - 1])left++; // check for duplicates
+                    while(left < right && nums[right] == nums[right + 1])right--; // check for duplicates
+                    
                 }
                 else if(sum < 0)left++;
-                else right--; 
+                else{
+                    right--;
+                }
             }
-        }
-        return ans;
+        }return ans;
     }
 };
